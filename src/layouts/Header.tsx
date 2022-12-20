2b4bfb977678356a4fe7f1ef5aa3366d1f07b1ba
gbertl <gilbertlcsndle@gmail.com>
2022-12-20 18:29:52 +0800

    implement login with auth0

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,6 +1,16 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import { useAuth0 } from '@auth0/auth0-react';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 
 const Header = () => {
+  const { isAuthenticated, loginWithPopup, logout } = useAuth0();
+
   return (
     <AppBar position="static" sx={{ mb: 5 }}>
       <Container>
@@ -21,6 +31,24 @@ const Header = () => {
           >
             Admin
           </Typography>
+
+          <Box sx={{ marginLeft: 'auto' }}>
+            {isAuthenticated ? (
+              <Button
+                sx={{ color: 'white', textTransform: 'uppercase' }}
+                onClick={() => logout()}
+              >
+                Logout
+              </Button>
+            ) : (
+              <Button
+                sx={{ color: 'white', textTransform: 'uppercase' }}
+                onClick={() => loginWithPopup()}
+              >
+                Log in
+              </Button>
+            )}
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import './index.css';
 import { createTheme } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Auth0ProviderWithHistory from './auth/Auth0ProviderWithHistory';
 
 const theme = createTheme({
   palette: {
@@ -18,10 +20,14 @@ const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Router>
+      <Auth0ProviderWithHistory>
+        <QueryClientProvider client={client}>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </Auth0ProviderWithHistory>
+    </Router>
   </React.StrictMode>
 );
