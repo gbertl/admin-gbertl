730de6ed0bc7226ac27897f68f1e382a5afb6aa6
gbertl <gilbertlcsndle@gmail.com>
2023-06-05 17:07:23 +0800

    remove leading /api

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = `${import.meta.env.VITE_SERVER_URL}/api`;
+const baseURL = `${import.meta.env.VITE_SERVER_URL}`;
 
 const instance = axios.create({
   baseURL,
